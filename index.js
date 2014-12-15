b6fefe35042d0d3e4fe5f4928364a0765b803d34
taterbase <shankga@gmail.com>
2014-12-15 13:08:19 -0700

    Do array things in series

--- index.js
+++ index.js
@@ -1,26 +1,42 @@
 module.exports = garr
 
 function garr(generator) {
-  return function(arguments) {
-    var g = new generator(arguments)
-    handleGenerator(g, null, false)
+  var iteratorIndex = -1
+    , iterators = []
+
+  var handleGenerator = function(generator, asyncFunc, done) {
+    if (done) {
+      var nextFunc = iterators[iteratorIndex].shift()
+      if (nextFunc !== undefined) {
+        nextFunc()
+      }
+      return
+    } if (asyncFunc) {
+      asyncFunc(function(err, result) {
+        if (err)
+          generator.throw(err)
+        else {
+          var result = generator.next(result)
+          handleGenerator(generator, result.value, result.done)
+        }
+      })
+    } else {
+      var result = generator.next()
+      handleGenerator(generator, result.value, result.done)
+    }
   }
+
+  var iterator = function(arguments) {
+    var g = generator(arguments)
+    var numOfGenerators = iterators[iteratorIndex].push(handleGenerator.bind(null, g, null, false))
+    if (numOfGenerators === 1) {
+      (iterators[iteratorIndex].shift())()
+    }
+  }
+
+  iteratorIndex = iterators.push(iterator) - 1
+  iterators[iteratorIndex] = []
+
+  return iterator
 }
 
-function handleGenerator(generator, asyncFunc, done) {
-  if (done) {
-    return
-  } if (asyncFunc) {
-    asyncFunc(function(err, result) {
-      if (err)
-        generator.throw(err)
-      else {
-        var result = generator.next(result)
-        handleGenerator(generator, result.value, result.done)
-      }
-    })
-  } else {
-    var result = generator.next()
-    handleGenerator(generator, result.value, result.done)
-  }
-}
